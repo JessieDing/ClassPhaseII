13f42cb883cbe65adecad56949d568afe4112d04
JessieDing <nathalie.ding@gmail.com>
2017-03-31 17:21:55 +0800

    File I/O study

--- src/d0331/IOTest.java
+++ src/d0331/IOTest.java
@@ -1,9 +1,6 @@
 package d0331;
 
-import java.io.BufferedOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
+import java.io.*;
 
 /**
  * Created by Administrator on 2017/3/31.
@@ -14,7 +11,9 @@ public class IOTest {
 //            testFOS();
 //            testFIS();
 //            testCopyFile();
-            testBOS();
+//            testBOS();
+//            testBIS();
+            testOOS();
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -57,7 +56,101 @@ public class IOTest {
         FileOutputStream fos = new FileOutputStream("e:" + File.separator + "fos.txt");
         BufferedOutputStream bos = new BufferedOutputStream(fos);
         bos.write("BufferedOutputStream test message.".getBytes());
-        bos.flush();//显示写入
+        bos.flush();//显示写入  ??作用
         bos.close();
+    }
+
+    public static void testBIS() throws Exception {
+        //创建缓冲字节输入流
+        FileInputStream fis = new FileInputStream("e:" + File.separator + "bisDemo.txt");
+        BufferedInputStream bis = new BufferedInputStream(fis);
+        int tmp = -1;
+        //缓冲读入，实际上并非是一个字节一个字节读入的
+        while ((tmp = bis.read()) != -1) {
+            System.out.print((char) tmp);
+        }
+        bis.close();
+    }
+
+
+    public static void testOOS() throws Exception {
+        //对象序列化示例
+        FileOutputStream fos = new FileOutputStream("e:" + File.separator + "emp.obj");
+        ObjectOutputStream oos = new ObjectOutputStream(fos);
+        Emp emp = new Emp("张三", "0001", "男", 29);
+        oos.writeObject(emp);
+        System.out.println("序列化完毕");
+        oos.close();
+        fos.close();
+
+        //对象反序列化
+        FileInputStream fis = new FileInputStream("e:" + File.separator + "emp.obj");
+        ObjectInputStream ois = new ObjectInputStream(fis);
+        Emp emp1 = (Emp) ois.readObject();
+        System.out.println(emp1.toString());
+        System.out.println("反序列化完毕");
+        ois.close();
+        fis.close();
+    }
+}
+
+class Emp implements Serializable {//该类实现Serializable接口，可进行序列化
+    private String name;
+    private String id;
+    private String gender;
+    private int age;
+    private static final long serialVersionUID = 1L;
+
+    public Emp() {
+    }
+
+    public Emp(String name, String no, String gender, int age) {
+        this.name = name;
+        this.id = no;
+        this.gender = gender;
+        this.age = age;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Emp{" +
+                "name='" + name + '\'' +
+                ", id='" + id + '\'' +
+                ", gender='" + gender + '\'' +
+                ", age=" + age +
+                '}';
     }
 }
